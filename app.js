6a9a29b3b8721dbaaebdd3485d020dcafef9a070
javeria-farooq <gettingeducation6@gmail.com>
2024-01-24 22:18:01 +0500

    24 JS Assignment

---
+++ app.js
@@ -0,0 +1,53 @@
+let key = prompt("Enter key what you want to set")
+let value = prompt("Enter value what you want to set")
+localStorage.setItem(key, value)
+
+
+
+
+
+// let myJson = {
+//     " name ": "javeria ",
+//     " score ": 100,
+//     " Is admin ": false
+// }
+
+
+
+// myJson = JSON.stringify(myJson)
+// localStorage.setItem('Email', myJson)
+// localStorage.setItem('Email', myJson)
+
+// let mySecJson = {
+//     " name ": "aliza",
+//     " score ": 190,
+//     " is admin ": true
+// }
+
+// mySecJson = JSON.stringify(mySecJson)
+// localStorage.setItem('Email', mySecJson)
+
+
+
+// localStorage.clear('Password')
+
+// console.log(myJson)
+// let mySecJson = JSON.stringify(myJson)
+// document.write(mySecJson)
+// localStorage.setItem("Email: ", mySecJson)
+// localStorage.setItem("Password: ", mySecJson)
+// localStorage.setItem("Email: ", mySecJson)
+
+// let vari = "Javeria"
+// localStorage.setItem('Name: ', vari)
+
+// let arr = ['jav', 'aliza', 'afnan']
+// localStorage.setItem('Name: ', arr)
+
+// localStorage.setItem('Name: ', myJson)
+
+// function btn() {
+//     let sEmail = document.getElementById('sEmail').value
+//     let myJson = JSON.stringify(sEmail)
+//     localStorage.setItem('first-Email: ', sEmail)
+// }
